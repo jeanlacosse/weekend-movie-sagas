2ac104f2724c58d61e77903027021c40252f71c3
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-24 16:38:30 -0500

    base functionality working

--- TODO.md
+++ TODO.md
@@ -1,10 +1,10 @@
 [] movie poster onClick send to /details
-    [] do with history.push router
-        [] history page shows all movie genres, title, desciption, img
-            [] sent from server, GET req to that specific movie from .map
-                [] run saga post to index and store in store
-        [] has back to list button to bring back to homepage
-            [] also use history.push here in routes
+    [x] do with history.push router
+        [x] history page shows all movie genres, title, desciption, img
+            [x] sent from server, GET req to that specific movie from .map
+                [x] run saga post to index and store in store
+        [x] has back to list button to bring back to homepage
+            [x] also use history.push here in routes
 
 [] material UI
     [] add cards to posters on list page
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,23 +1,38 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, Link } from 'react-router-dom';
 
 function MovieDetails() {
 
+    const history = useHistory();
     const movieDetails = useSelector(store => store.movieDetails);
     console.log('movieDetails', movieDetails)
 
     return (
         <>
-        {/* conditional rendering on page load to see if the array for movieDetails
+            {/* conditional rendering on page load to see if the array for movieDetails
         is empty or not, if not empty it will append info to dom */}
             {movieDetails.length !== 0 &&
+
                 <div>
-                    <h1>here is some stuff</h1>
-                    <p>{movieDetails[0].title}
+
+                    <h2>{movieDetails[0].title}</h2>
+                    <h3>Genres:
+                        {movieDetails[0].all_genres.map((genre) => {
+                            return (
+                                <span key={genre}>{genre} </span>
+                            )
+                        })}
+                    </h3>
+                    <p>
                         {movieDetails[0].description}
-                        <img src={movieDetails[0].poster} alt="" />
                     </p>
+                    <img src={movieDetails[0].poster} alt="" />
+
                 </div>
             }
+            <button
+                onClick={() => { history.push('/') }}>
+                Go back to list</button>
         </>
     )
 }
